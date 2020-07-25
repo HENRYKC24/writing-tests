46ec5c95af3dcf5766dd1272de91fa0804d257e1
HENRY-KC <henrykc24@yahoo.com>
2020-07-25 17:47:32 +0100

    implement a server

--- program.js
+++ program.js
@@ -1,7 +1,21 @@
 var book = require("./lib/grades").book;
 
-for (var i = 2; i < process.argv.length; i += 1) {
-  book.addGrade(parseInt(process.argv[i]));
-}
+var express = require('express');
+var app = express();
 
-console.log(book.getAverage());
+app.get('/', function (req, res) {
+  res.send('Hello, World!');
+});
+
+app.get('/grades', function(req, res) {
+  var grades = req.query.grade.split(',');
+  for(var i = 0; i < grades.length; i += 1) {
+    book.addGrade(parseInt(grades[i]));
+  };
+  var average = book.getAverage();
+  var grade = book.getLetterGrade();
+  res.end('Your average grade is ' + average + '. That\'s an/a ' + grade);
+});
+
+app.listen(3000);
+console.log('Server is setup...');
